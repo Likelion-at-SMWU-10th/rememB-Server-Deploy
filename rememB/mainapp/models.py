b39d98a2673fe5b4c3a2a609ac0dfbdffadbb59b
happine2s <happine2s@sookmyung.ac.kr>
2022-07-21 16:29:03 +0900

    [feat] 카카오 로그인

--- rememB/mainapp/models.py
+++ rememB/mainapp/models.py
@@ -1,6 +1,4 @@
 from django.db import models
-<<<<<<< HEAD
-=======
 
 class User(models.Model):
     uuid = models.CharField(max_length=100)
@@ -9,4 +7,3 @@ class User(models.Model):
     birthday = models.CharField(max_length=100)
     email = models.CharField(max_length=100)
 
->>>>>>> develop

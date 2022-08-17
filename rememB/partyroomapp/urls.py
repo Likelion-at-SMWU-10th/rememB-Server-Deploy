1f3725f8632b78c04670ee88ce2dd7b7ca7cdff4
happine2s <happine2s@sookmyung.ac.kr>
2022-08-18 01:21:23 +0900

    [fix] remove turtle

--- rememB/partyroomapp/urls.py
+++ rememB/partyroomapp/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('<int:userpk>/',views.UserLetterView.as_view()), #userpk의 편지만 조회
+     path('<int:userpk>/',views.UserLetterView.as_view()), #userpk의 편지만 조회
 ]
 

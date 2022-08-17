104c6dfd12ab6111ba988daf8918190ebce389bc
summit45 <brandy2145@gmail.com>
2022-08-18 03:01:33 +0900

    [fix] partyroom 수정

--- rememB/balanceapp/serializers.py
+++ rememB/balanceapp/serializers.py
@@ -1,21 +1,65 @@
+from datetime import datetime
 from rest_framework import serializers
 from .models import *
 from datetime import datetime
 from userapp.models import User
+
+def getDayBefore(mybirthday):
+    mybirthdayList = mybirthday.split("-")
+    byear = int(mybirthdayList[0])
+    bmonth = int(mybirthdayList[1])
+    bday = int(mybirthdayList[2])
+    
+    nowList = str(datetime.now().date()).split("-")
+    nmonth = int(nowList[1])
+    nday= int(nowList[2])
+
+    if(bmonth<nmonth | ((bmonth==nmonth) & (bday<nday))): #이미 생일이 지난경우
+        dday = datetime(2023, bmonth, bday).date()
+        now = datetime.now().date()
+        print(str(dday-now).split(",")[0].split(" ")[0])
+        return int(str(dday-now).split(",")[0].split(" ")[0])
+    else:
+        dday = datetime(2022, bmonth, bday).date()
+        print(dday)
+        now = datetime.now().date()
+        diff = str(dday-now).split(",")[0].split(" ")[0]
+        print(diff)
+        return int(diff)
+
 
 class QuestionSerializer(serializers.ModelSerializer):
     class Meta:
         model=Question
         fields='__all__'
 
+
 class AnswerSerializer(serializers.ModelSerializer):
     class Meta:
         model=Answer
         fields='__all__'
 
+
+class BalancePostSerializer(serializers.ModelSerializer):
+    leftDay=serializers.SerializerMethodField()
+
+    class Meta:
+        model=Balance
+        fields = (
+            'user',
+            'leftDay',
+            'question_id',
+            'answer_id',
+        )
+    
+    def get_leftDay(self, obj):
+        return getDayBefore(str(User.objects.get(id=obj.user.id).birth))
+
+
 class BalanceSerializer(serializers.ModelSerializer):
     qcontent=serializers.SerializerMethodField()
-    acontent=serializers.SerializerMethodField()
+    acontent1=serializers.SerializerMethodField()
+    acontent2=serializers.SerializerMethodField()
 
     class Meta:
         model=Balance
@@ -24,15 +68,19 @@ class BalanceSerializer(serializers.ModelSerializer):
             'question_id',
             'answer_id',
             'qcontent',
-            'acontent'
+            'acontent1',
+            'acontent2'
         )
 
     #유효하지 않은 값이 들어왔을 때 오류처리해야함
     def get_qcontent(self, obj):
         return Question.objects.get(id=obj.question_id.id).question_content
+       
+    def get_acontent1(self, obj): 
+        return Answer.objects.get(id=obj.question_id.id*2-1).answer_content
     
-    def get_acontent(self, obj): 
-        return Answer.objects.get(id=obj.answer_id.id).answer_content
+    def get_acontent2(self, obj): 
+        return Answer.objects.get(id=obj.question_id.id*2).answer_content
 
 
 class BAQSerializer(serializers.ModelSerializer):
@@ -56,12 +104,17 @@ class BAQSerializer(serializers.ModelSerializer):
 
     def get_a1id(self, obj):
         return Answer.objects.get(id=(obj.id*2-1)).id
-    
+       
     def get_a1content(self, obj): 
         return Answer.objects.get(id=(obj.id*2-1)).answer_content
     
     def get_a2id(self, obj):
         return Answer.objects.get(id=(obj.id*2)).id
-    
+       
     def get_a2content(self, obj): 
         return Answer.objects.get(id=(obj.id*2)).answer_content
+
+
+    
+    
+        
--- rememB/balanceapp/views.py
+++ rememB/balanceapp/views.py
@@ -7,11 +7,37 @@ from rest_framework import permissions
 from userapp.authenticate import SafeJWTAuthentication
 
 from .models import Answer, Balance, Question
-from .serializers import AnswerSerializer, BAQSerializer, QuestionSerializer, BalanceSerializer
+from .serializers import AnswerSerializer, BAQSerializer, BalancePostSerializer, QuestionSerializer, BalanceSerializer
 from userapp.models import User
+
+def getDayBefore(mybirthday):
+    mybirthdayList = mybirthday.split("-")
+    byear = int(mybirthdayList[0])
+    bmonth = int(mybirthdayList[1])
+    bday = int(mybirthdayList[2])
+    
+    nowList = str(datetime.now().date()).split("-")
+    nmonth = int(nowList[1])
+    nday= int(nowList[2])
+
+    if(bmonth<nmonth | ((bmonth==nmonth) & (bday<nday))): #이미 생일이 지난경우
+        dday = datetime(2023, bmonth, bday).date()
+        now = datetime.now().date()
+        #print(str(dday-now).split(",")[0].split(" ")[0])
+        return int(str(dday-now).split(",")[0].split(" ")[0])
+    else:
+        dday = datetime(2022, bmonth, bday).date()
+        #print(dday)
+        now = datetime.now().date()
+        diff = str(dday-now).split(",")[0].split(" ")[0]
+        #print(diff)
+        return int(diff)
 
 
 class QuestionList(APIView):
+    authentication_classes=[SafeJWTAuthentication]
+    permission_classes=[permissions.IsAdminUser]
+
     def get(self, request): #작성한 모든 질문 보기
         questions=Question.objects.all()
         serializers=QuestionSerializer(questions, many=True)
@@ -26,6 +52,9 @@ class QuestionList(APIView):
 
 
 class AnswerList(APIView):
+    authentication_classes=[SafeJWTAuthentication]
+    permission_classes=[permissions.IsAdminUser]
+
     def get(self, request): #작성한 모든 답 보기
         answers=Answer.objects.all()
         serializers=AnswerSerializer(answers, many=True)
@@ -40,6 +69,9 @@ class AnswerList(APIView):
 
 
 class BalanceList(APIView): #user(pk)의 질문&대답 목록 
+    authentication_classes=[SafeJWTAuthentication]
+    permission_classes=[permissions.IsAdminUser]
+
     def get(self, request, pk): #한 유저의 전체 질문&대답 fk 전해주기
         balances=Balance.objects.filter(id=pk)
         serializers=BalanceSerializer(balances,many=True)
@@ -53,7 +85,7 @@ class BalanceList(APIView): #user(pk)의 질문&대답 목록
 # 2. 7일 이내라면 해당 질문과 답을 보여준다. / 밀린 게 있을 수도 있으니 다수 가능
 # 3. 해당 답을 받아온다.
 
-#실제 프론트와 전달할 api
+#/balance/list/<유저pk>
 class myBalanceList(APIView): #user(pk)의 질문&대답 목록 
     authentication_classes=[SafeJWTAuthentication]
     permission_classes=[permissions.AllowAny]
@@ -64,41 +96,42 @@ class myBalanceList(APIView): #user(pk)의 질문&대답 목록
         serializers=BalanceSerializer(balances,many=True)
         return Response(serializers.data, status=status.HTTP_200_OK)
 
+#/balance/ans/<질문번호>(=남은날짜)
 class myBalanceGame(APIView):
     authentication_classes=[SafeJWTAuthentication]
-    permission_classes=[permissions.AllowAny]
+    permission_classes=[permissions.IsAuthenticated]
     
     def post(self, request, pk): #밸런스게임 질문-답 선택(질문 형식으로)
-        user=User.objects.get(id=pk)
-        leftDay=User.getDayBefore(str(user.birth))
-        print(request.data)
-        if(leftDay <=7):
-            request.data['user'] = user.id
-            serializer = BalanceSerializer(data=request.data)
-            
-            if serializer.is_valid():
-                serializer.save()
-                return Response(serializer.data, status=status.HTTP_201_CREATED)
-            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        userobj=User.objects.get(id=request.data['user'])
+        leftDay=getDayBefore(str(userobj.birth))
+        # print("D-DAY", leftDay)
+        if(leftDay == pk): #오늘의 밸런스 게임
+            try: #이미 했다면
+                balanceobj = Balance.objects.filter(user=userobj).get(question_id=pk)
+                return Response('이미 참여했습니다.', status=status.HTTP_200_OK)
+            except Balance.DoesNotExist: #안했으면
+                if((request.data['question_id']==str(pk)) & ( (request.data['answer_id']==str(pk*2-1)) | (request.data['answer_id']==str(pk*2)))):
+                    serializer = BalancePostSerializer(data=request.data)
+                    if serializer.is_valid():
+                        serializer.save()
+                        return Response(serializer.data, status=status.HTTP_200_OK)
+                    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+                else:
+                    return Response('질문/응답 번호가 잘못되었습니다.',status=status.HTTP_200_OK )
+
+        elif(leftDay < pk): #지난 밸런스게임
+            try: #이미 했다면
+                balanceobj = Balance.objects.filter(user=userobj).get(question_id=pk)
+                return Response('이미 참여했습니다.', status=status.HTTP_200_OK)
+            except Balance.DoesNotExist: #안했으면
+                if((request.data['question_id']==str(pk)) & ( (request.data['answer_id']==str(pk*2-1)) | (request.data['answer_id']==str(pk*2)))):
+                    serializer = BalancePostSerializer(data=request.data)
+                    if serializer.is_valid():
+                        serializer.save()
+                        return Response(serializer.data, status=status.HTTP_200_OK)
+                    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+                else:
+                    return Response('질문/응답 번호가 잘못되었습니다.',status=status.HTTP_200_OK )
         else:
-            return Response("아직 응답 못함", status=status.HTTP_200_OK)
-
-        
-    def get(self, request, pk): #밸런스게임 질문-답 선택할수있도록 
-        user=User.objects.get(id=pk)
-        leftDay=User.getDayBefore(str(user.birth))
-
-        if(leftDay <= 7): #생일 비교해보고 7일 이내라면
-            questions=Question.objects.filter(id__gt=(leftDay-1)) #쿼리셋
-            print(questions)
-            serializer = BAQSerializer(questions, many=True)
-            return Response(serializer.data, status=status.HTTP_200_OK)
-            # if serializer.is_valid():
-            #     return Response(serializer.data, status=status.HTTP_200_OK) #질문이랑 데이터가 전달됨 
-            # return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-            
-        else: #7일 이내가 아니라면 아직 답을 확인할 수 없음
-            print("not yet")
-            return Response("not yet", status=status.HTTP_200_OK)
-
+            return Response("D-day 해당하지 않음", status=status.HTTP_200_OK)
 
